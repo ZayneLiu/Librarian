9f5c01d5d53c31b1ececbab445283afc46aa6a02
Zayne.Liu <llz981011@hotmail.com>
2020-07-14 02:45:00 +0100

    Add XML doc support for swagger ui.

--- CustodianWebAPI/Startup.cs
+++ CustodianWebAPI/Startup.cs
@@ -33,7 +33,11 @@ namespace CustodianWebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 
-
+                // Important!!
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
