2117118b321b55bcfcc4c3bc9957c2634ecc6793
Zayne.Liu <llz981011@hotmail.com>
2020-07-14 16:45:59 +0100

    add axios and vuex

--- librarian-ui-nw-vue/src/main.ts
+++ librarian-ui-nw-vue/src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import mitt from "mitt";
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false;
 export const mitter = mitt();
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');
