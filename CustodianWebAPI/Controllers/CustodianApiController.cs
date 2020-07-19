e76f5357b20aae6920c28ef57d3b7e91a3fe2adb
Zayne.Liu <llz981011@hotmail.com>
2020-07-19 23:27:33 +0100

    web api implementation

--- CustodianWebAPI/Controllers/CustodianApiController.cs
+++ CustodianWebAPI/Controllers/CustodianApiController.cs
@@ -1,4 +1,8 @@
-using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CustodianAPI;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CustodianWebAPI.Controllers
@@ -7,29 +11,88 @@ namespace CustodianWebAPI.Controllers
     [Route("/")]
     public class CustodianApiController : Controller
     {
-        // GET
+        public static readonly Custodian Custodian = new Custodian();
+        public static readonly char PathDelimiter = Path.DirectorySeparatorChar;
+
+
+        public class FolderResult
+        {
+            public string FolderName { get; set; }
+            public string FolderPath { get; set; }
+        }
+
         /// <summary>
-        /// Get indexed folders
+        /// Get indexed folders.
         /// </summary>
         /// <returns>List of indexed folders</returns>
         [HttpGet("folders")]
-        public IActionResult GetIndexedFolders()
+        public ActionResult<List<FolderResult>> GetIndexedFolders()
         {
-            return new JsonResult(new[] {"a"});
+            var result = new List<FolderResult>();
+
+            var folders = Custodian.Folders;
+            folders.ForEach(folder =>
+                result.Add(new FolderResult
+                    {FolderName = folder.Location.Split(PathDelimiter).Last(), FolderPath = folder.Location})
+            );
+            return result;
         }
 
+
+        /// <summary>
+        /// Index given folder.
+        /// </summary>
+        /// <param name="folderPath">Path to the folder to be indexed.</param>
+        /// <returns>Indexed folder info.</returns>
         [HttpPost("folders")]
         public IActionResult IndexGivenFolder(string folderPath)
         {
+            var result = Custodian.TakeCareOf(folderPath);
+            // Console.WriteLine();
+            var folderName = result.Location.Split(PathDelimiter).Last();
+
             return new JsonResult(
-                new {msg=$"folder {folderPath} is indexed."}
+                new
+                {
+                    folder = new {location = result.Location, files = result.Documents.Count},
+                    msg =
+                        $"folder {folderName} is indexed with {result.Documents.Count} in total."
+                }
             );
         }
 
-        [HttpGet()]
-        public IActionResult Search(string keyword)
+        public class DocumentResult
         {
-            return new JsonResult(new {result = "aaa"});
+            public string DocumentName { get; set; }
+            public Dictionary<string, int> Result { get; set; }
+        }
+
+        [HttpPost("search")]
+        public ActionResult<List<DocumentResult>> Search(string keyword)
+        {
+            keyword = keyword.ToLower();
+            var result = new List<DocumentResult>();
+            var keywords = keyword.Split(" ");
+
+            Console.WriteLine(Custodian.Search(keywords));
+            using var resultDocList = Custodian.Search(keywords).GetEnumerator();
+            while (resultDocList.MoveNext())
+            {
+                var currentDoc = resultDocList.Current;
+                if (currentDoc == null)
+                    throw new Exception("No Doc!");
+
+                var resultDict = keywords.ToDictionary(kw => kw, kw => currentDoc.Thumbnail[kw]);
+                result.Add(
+                    new DocumentResult()
+                    {
+                        DocumentName = currentDoc.Name,
+                        Result = resultDict
+                    }
+                );
+            }
+
+            return result;
         }
     }
 }
