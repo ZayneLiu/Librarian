c18b46ffa40448b6015fcc0e2d32c4c40eb5a8e4
Zayne.Liu <llz981011@hotmail.com>
2020-08-01 17:35:55 +0100

    update open-xml-sdk and unit tests

--- CustodianAPI/Test/WordDocumentTest.cs
+++ CustodianAPI/Test/WordDocumentTest.cs
@@ -8,12 +8,14 @@ namespace CustodianAPI.Test
         [Fact]
         public void IndexTest()
         {
-            var testDocx = new Word07Document("/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/EazyCyber.docx");
             // var testOdt = new WordDocument("/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/test.odt");
-            var testDoc = new Word97Document("/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/test.doc");
+            // Word 2007 (.docx, .docm)
+            var testDocx = new Word07Document("/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/EazyCyber.docx");
+            // Word 1997 (.doc)
+            // var testDoc = new Word97Document("/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/test.doc");
 
             // Assert.Equal(3, testOdt.Thumbnail["add"]);
-            Assert.Equal(3, testDoc.Thumbnail["add"]);
+            // Assert.Equal(3, testDoc.Thumbnail["add"]);
         }
     }
 }
