ec5849f897b93986d860993a548f0253ee333506
Zayne.Liu <llz981011@hotmail.com>
2020-08-03 22:29:38 +0100

    Rename SharedTestData

--- CustodianAPI/Test/SharedTestData.cs
+++ CustodianAPI/Test/SharedTestData.cs
@@ -1,6 +1,6 @@
 namespace CustodianAPI.Test
 {
-    public class SharedTest
+    public class SharedTestData
     {
         public static string TestDocFolderPath = "/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/";
     }
