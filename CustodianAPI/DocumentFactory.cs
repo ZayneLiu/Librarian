9f70971978ded4fca5480ca5f5f80b43d4dcc6cc
Zayne.Liu <llz981011@hotmail.com>
2020-08-04 01:54:00 +0100

    Add `AllowedExtensions` property to retrieve all supported extensions

--- CustodianAPI/DocumentFactory.cs
+++ CustodianAPI/DocumentFactory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using CustodianAPI.Utils;
@@ -6,33 +7,42 @@ namespace CustodianAPI
 {
     public static class DocumentFactory
     {
+        public static string[] AllowedExtentions
+        {
+            get
+            {
+                return TextDocExtensions.Concat(WordExtensions)
+                                        .Concat(PowerPointExtentions)
+                                        .Concat(ExcelExtension)
+                                        .Concat(PdfExtensions)
+                                        .ToArray();
+            }
+            set { }
+        }
+        // Text Document
+        public static string[] TextDocExtensions = new[] { ".txt", };
+        // MS Word Document
+        public static string[] WordExtensions = new[] { ".docx", ".docm" };
+        // MS PowerPoint Documents
+        public static string[] PowerPointExtentions = new[] { ".pptx", ".pptm" };
+        // MS Excel Documents
+        public static string[] ExcelExtension = new[] { ".xlsx", ".xlsm" };
+        // PDF Document
+        public static string[] PdfExtensions = new[] { ".pdf" };
         public static Document Create(string path)
         {
             var ext = Path.GetExtension(path);
-            // Text Document
-            var textDocExtensions = new[] { ".txt", };
-            // MS Word Document
-            var wordDocExtensions = new[] { ".doc", ".docx" };
-            // PDF Document
-            var pdfDocExtensions = new[] { ".pdf" };
 
-            if (textDocExtensions.Contains(ext))
-            {
+            if (TextDocExtensions.Contains(ext))
                 return new TextDocument(path);
-            }
-
-            if (wordDocExtensions.Contains(ext))
-            {
-                if (ext == ".doc")
-                    return new Word1997Document(path);
-                else
-                    return new Word2007Document(path);
-            }
-
-            if (pdfDocExtensions.Contains(ext))
-            {
+            else if (WordExtensions.Contains(ext))
+                return new Word2007Document(path);
+            else if (PowerPointExtentions.Contains(ext))
+                return new PowerPoint2007Document(path);
+            else if (ExcelExtension.Contains(ext))
+                return new Excel2007Document(path);
+            else if (PdfExtensions.Contains(ext))
                 return new PdfDocument(path);
-            }
 
             return null;
         }
