f22f2262e1f5707d856ac809558e9b4b175722a7
Zayne.Liu <llz981011@hotmail.com>
2020-08-09 13:04:45 +0100

    get project directory for test docs

--- CustodianAPI/Test/SharedTestData.cs
+++ CustodianAPI/Test/SharedTestData.cs
@@ -1,8 +1,20 @@
+using System.IO;
+
 namespace CustodianAPI.Test
 {
     public class SharedTestData
     {
-        public static string TestDocFolderPath = "/Users/zayne/Workspace/Desktop/Librarian/CustodianAPI/Test Doc/";
+        public static string ProjectDirectory
+        {
+            get
+            {
+                // Get path to "./bin/Debug/netcoreapp3.1"
+                var currentWorkingDirectory = Directory.GetCurrentDirectory();
+                // Get project path "./"
+                return Directory.GetParent(currentWorkingDirectory).Parent.Parent.FullName;
+            }
+        }
+        public static string TestDocFolderPath = $"{ProjectDirectory}/Test Doc/";
     }
 
 }
