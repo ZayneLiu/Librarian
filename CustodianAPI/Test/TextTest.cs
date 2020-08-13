50da78c4a789d84620c2ad2d65c010b43bfa49c5
Zayne.Liu <llz981011@hotmail.com>
2020-08-13 21:04:50 +0100

    txt test

---
+++ CustodianAPI/Test/TextTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using CustodianAPI.Utils;
+
+namespace CustodianAPI.Test
+{
+    public class TextTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            //Given
+            var txt = new TextDocument(SharedTestData.TestDocFolderPath + "test.txt");
+            //When
+
+            //Then
+            Assert.Equal(30, txt.Thumbnail["castle"]);
+        }
+    }
+}
